04a2ae8bca12f02449298e6f3754c757ca35a37a
Faizal Hassan <faizal_10@live.co.uk>
2021-04-23 04:24:28 +0100

    Super basic implementation of the Translator Cognitive Service.

--- src/bot.js
+++ src/bot.js
@@ -1,10 +1,11 @@
 const tmi = require('tmi.js');
+const axios = require('axios').default;
 require('dotenv').config();
 
 const opts = {
     identity: {
         username: process.env.BOT_USERNAME,
-        password: process.env.OAUTH_TOKEN
+        password: process.env.TWITCH_OAUTH
     },
     channels: [
         process.env.CHANNEL_NAME
@@ -32,12 +33,46 @@ function onMessageHandler (target, context, msg, self) {
     if (commandName === '!hello') {
       client.say(target, 'Hello World!');
       console.log(`* Executed ${commandName} command`);
-    } else {
-      console.log(`* Unknown command ${commandName}`);
+    } 
+    // else {
+    //   console.log(`* Unknown command ${commandName}`);
+    // }
+
+    if (commandName.includes("ping")) {
+      client.say(target, "pong")
     }
+
+    translateMessage(commandName, target);
+
   }
   
   // Called every time the bot connects to Twitch chat
-  function onConnectedHandler (addr, port) {
+  function onConnectedHandler(addr, port) {
     console.log(`* Connected to ${addr}:${port}`);
+  }
+
+  // Translate Message
+  function translateMessage(commandName, target) {
+    var subscriptionKey = process.env.AZURE_SUB_KEY;
+    var endpoint = "https://api.cognitive.microsofttranslator.com/translate";
+    axios({
+      baseURL: endpoint,
+      method: 'post',
+      headers: {
+          'Ocp-Apim-Subscription-Key': subscriptionKey,
+          'Content-type': 'application/json',
+      },
+      params: {
+          'api-version': '3.0',
+          // 'from': 'en',
+          'to': ['ja']
+      },
+      data: [{
+          'text': commandName
+      }],
+      responseType: 'json'
+  }).then(function(response){
+      console.log(JSON.stringify(response.data, null, 4));
+      return client.say(target, JSON.stringify(response.data[0].translations[0].text) )
+  });
   }
